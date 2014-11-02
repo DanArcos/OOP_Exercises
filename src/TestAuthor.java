6bdabb3f366a64c777a098d802c5076e5230a9eb
DanArcos <daniel.arcos@outlook.com>
2014-11-02 17:06:37 -0500

    Update TestAuthor.java

--- src/TestAuthor.java
+++ src/TestAuthor.java
@@ -1,4 +1,4 @@
-
+//This is will create an Author object and print out info on the object
 public class TestAuthor {
     public static void main (String[] args){
         Author anAuthor = new Author("Tan Ah Teck", "ahteck@somewhere.com", 'm');
