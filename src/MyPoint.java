d36099335e5966eccaae519435d38ac53e615f29
DanArcos <daniel.arcos@outlook.com>
2014-11-02 16:49:36 -0500

    Update MyPoint.java

--- src/MyPoint.java
+++ src/MyPoint.java
@@ -3,6 +3,7 @@ public class MyPoint {
     private int x;
     private int y;
 
+    //Add Constructors
     public MyPoint(){
         this.x = 0;
         this.y = 0;
@@ -13,6 +14,7 @@ public class MyPoint {
         this.y = y;
     }
 
+    //Add appropriate getters and setters
     public int getX(){
         return x;
     }
@@ -34,16 +36,20 @@ public class MyPoint {
         this.y = y;
     }
 
+    //Override toString
     public String toString(){
         return "("+x+","+y+")";
     }
 
+    //This calculates the distance between this object (Point's) location and another coordinate
     public double distance (int x, int y){
         int xDiff = this.x - x;
         int yDiff = this.y - y;
         return Math.sqrt(xDiff*xDiff+yDiff*yDiff);
     }
 
+    //This calculates the distance between this object (Point's) and another Point object
+    //This does the exact same thing as the previous method, however different parameters are introduced.
     public double distance(MyPoint another){
         int xDiff = this.x - another.x;
         int yDiff = this.y - another.y;
